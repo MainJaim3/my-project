39469050e394b8d8f15265020ed140ce04eb03ba
{uMainJaim3} <{jaime.moreno@mtp.es}>
2025-02-26 18:57:50 +0100

    Subida de día 3

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,3 +1,25 @@
-from ejercicio5 import respuesta
 
-respuesta = request.get()
+
+
+"https://the-internet.herokuapp.com/login"
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://the-internet.herokuapp.com/login")
+
+
+   tbx__username_master = driver.find_element(By.ID, value:"username")
+   tbx__username.send_keys("tomsmith")
+
+   tbx__password = driver.find_element(By.ID, value: "password")
+   tbx__password.send_keys("SuperSecretPassword!")
+
+   driver.find_element(By.CSS_SELECTOR, value: "button").click()
+
+   driver.save_screenshot("screenshot.png")
+
+finally:
+driver.quit()
+
